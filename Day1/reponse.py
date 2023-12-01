2b0ee9543c1373b386a86b05d3f3e73f8ebcdd0f
LaurentLienhard <laurent.lienhard@outlook.com>
2023-12-01 11:48:23 +0100

    WIN

--- Day1/reponse.py
+++ Day1/reponse.py
@@ -2,7 +2,38 @@
 with open('puzzle.txt') as f:
     data = f.readlines()
 
-#Read each line and concat the first digit and the last digit to a form a number
+#read each line and find the first numeric character and the last numeric character. if they are only one numeric character, then the first and last are the same
+#add the numeric characters together and print the sum
 
+
+
+
+
+
+
+
+sum = 0
+for line in data:
+    first_num = ""
+    last_num = ""
+    result = ""
+    for i in range(0,len(line)):
+        if line[i].isdigit():
+            first_num = int(line[i])
+            print("first_Num : ", first_num)
+            break
+    for i in range(len(line)-1,0,-1):
+        if line[i].isdigit():
+            last_num = int(line[i])
+            print("last_Num : ",  last_num)
+            break
+    #check if last_num is empty 
+    if last_num == "":
+        last_num = first_num
+    #concatenate first_num and last_num to make a number
+    result = int(str(first_num) + str(last_num))
+    print("result : ", result)
+    sum = sum + result
+print("sum : ", sum)
 
 
