ec18e0223424b09d1ce2f48d9db8f4902bf31d00
LaurentLienhard <laurent.lienhard@outlook.com>
2023-12-01 19:33:07 +0000

    add concat function

--- Day1/reponse.py
+++ Day1/reponse.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import re
 
 # Day1/Part1
@@ -37,35 +38,38 @@ regex = re.compile(pattern)
 
 def ConvertToNumeric(value):
     switcher = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
     }
     return switcher.get(value, "Invalid value")
+
+def numConcat(num1, num2):
+  return int("{}{}".format(num1, num2))
 
 sum = 0
 for line in data:
     first_num = ""
     last_num = ""
-    result = ""
+    result = 0
     # Test the regex pattern
     matches = regex.findall(line)
     # if matches is not a digit convert to numeric
     if not matches[0].isdigit():
         first_num = ConvertToNumeric(matches[0])
     else:
-        first_num = matches[0]
+        first_num = int(matches[0])
     if not matches[-1].isdigit():
         last_num = ConvertToNumeric(matches[-1])
     else:
-        last_num = matches[-1]
-    result = int(str(first_num) + str(last_num))
+        last_num = int(matches[-1])
+    result = numConcat(first_num, last_num)
     print("line : ", line)
     print("first_num: ", first_num)
     print("last_num: ", last_num)
