389a11dec117fded4085875408cb1d475138cad9
LaurentLienhard <laurent.lienhard@outlook.com>
2023-12-01 10:40:41 +0100

    premier essai

---
+++ Day1/reponse.py
@@ -0,0 +1,8 @@
+#Import data from file puzzle.txt in the current directory
+with open('puzzle.txt') as f:
+    data = f.readlines()
+
+#Read each line and concat the first digit and the last digit to a form a number
+
+
+
