2661d82064cf70df00d96bf809696f78d9b0b7d4
LaurentLienhard <laurent.lienhard@outlook.com>
2023-12-03 13:42:56 +0000

    hurry up save this little thing

--- Day3/reponse.py
+++ Day3/reponse.py
@@ -1,18 +1,28 @@
+from math import fabs
 import re
 
 def Day3part1():
     print("Day3 part1")
 
     # Read all the data from the file
-    file = open("Day3/puzzle.txt", "r")
+    file = open("Day3/test.txt", "r")
     data = file.read()
 
+    sum = 0
     # Find all numbers in the data
     numbers = re.findall(r'\d+', data)
-
-    # Check if each digit of the number is adjacent to a symbol different than a point
     for number in numbers:
-        for digit in number:
-            print(number)
-            print(digit)
+        for digit_position, digit in enumerate(number):
+            digital_ablolute_position = data.index(number) + digit_position
+
+            if (not (re.search(r'[0-9. ]', data[digital_ablolute_position - 1]))) or (not (re.search(r'[0-9. ]', data[digital_ablolute_position + 1]))):
+                print('Number : '  + number)
+                print('Digit : ' + digit)
+                print('charactere before digit : ' + data[digital_ablolute_position - 1])
+                print('charactere after digit : ' + data[digital_ablolute_position + 1])
+                print('not Point or digit before digit')
+                sum += int(number)
+    print('sum : ' + str(sum))
+
+    
 Day3part1()
